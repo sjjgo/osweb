25b528167a13d35661615e5ce01640ce8cd646b4
smathot <s.mathot@cogsci.nl>
2021-08-05 15:57:09 +0200

    Add documentation to the Canvas handler and common functions

--- src/js/osweb/classes/canvas_handler.js
+++ src/js/osweb/classes/canvas_handler.js
@@ -2,8 +2,34 @@ import Styles from '../backends/styles.js'
 import Canvas from '../backends/canvas.js'
 
 /**
- * A class that provides a more user-friendly API for Canvas objects, resemling
- * that of Python. Is used for the JavaScript workspace.
+ * The `Canvas` class is used to present visual stimuli. You generally
+ * create a `Canvas` object with the `Canvas()` factory function. Because
+ * `Canvas()` is a function, you do *not* need to use `new` when calling it.
+ * The JavaScript `Canvas` class mimicks the corresponding Python `Canvas`
+ * class.
+ * 
+ * __Style keywords__ can be passed to all functions that accept `styleArgs`.
+ * Style keywords can also be set as properties of the `Canvas` object. For an
+ * overview of style keywords, see the
+ * [Python `Canvas` documentation](%url:manual/python/canvas%).
+ * 
+ * __Important:__ JavaScript doesn't support named parameters (or: keywords).
+ * Therefore, parameters are passed an `Object` with named properties and
+ * default values. Like so:
+ *
+ * ```js
+ * var myCanvas = Canvas()
+ * // (correct) pass parameters as an Object ...
+ * myCanvas.fixdot({color: 'red'})
+ * // (incorrect) ... and *not* as named parameters
+ * // myCanvas.fixdot(color='red')
+ * myCanvas.show()
+ * ```
+ *
+ * [TOC]
+ *
+ * @class
+ * @name Canvas
  **/ 
 export default class CanvasHandler {
 
@@ -44,15 +70,32 @@ export default class CanvasHandler {
     return style
   }
   
-  arrow({
-      sx = 0,
-      sy = 0,
-      ex = 50,
-      ey = 0,
-      body_length = 0.8,
-      body_width = 0.5,
-      head_width = 30,
-      ...styleArgs} = {}) {
+  /**
+   * Draws an arrow. An arrow is a polygon consisting of 7 vertices, with an
+   * arrowhead pointing at (ex, ey).
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * var w = vars.width / 2
+   * var h = vars.height / 2
+   * // Important: parameters are passed as an Object
+   * myCanvas.arrow({sx: 0, sy: 0, w: w, h: h, head_width:100, body_length:0.5})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.sx=0
+   * @param {Number} obj.sy=0
+   * @param {Number} obj.ex=50
+   * @param {Number} obj.ey=0
+   * @param {Number} obj.body_length=0.8
+   * @param {Number} obj.body_width=0.5
+   * @param {Number} obj.head_width=30
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.arrow
+   * @function
+   **/
+  arrow({sx = 0, sy = 0, ex = 50, ey = 0, body_length = 0.8, body_width = 0.5,
+      head_width = 30, ...styleArgs} = {}) {
     this._canvas.arrow(
         sx + this._xc,
         sy + this._yc,
@@ -64,10 +107,45 @@ export default class CanvasHandler {
         this._element_style(styleArgs))
   }
   
+  /**
+   *	 Clears the canvas with the current background color. Note that it is
+   *	 generally faster to use a different canvas for each experimental
+   *	 display than to use a single canvas and repeatedly clear and redraw
+   *	 it.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.fixdot({color: 'green'})
+   * myCanvas.show()
+   * // do something
+   * myCanvas.clear()
+   * myCanvas.fixdot({color: 'red'})
+   * myCanvas.show()
+   *
+   * @param {Object} [styleArgs={}]
+   * @name Canvas.clear
+   * @function
+   **/
   clear(styleArgs = {}) {
     this._canvas.clear(this._element_style(styleArgs).background_color)
   }
   
+  /**
+   * Draws a circle.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.circle({x: 100, y: 100, r: 50, fill: true, color:'red'})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {Number} obj.r=50
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.circle
+   * @function
+   **/
   circle({x = 0, y = 0, r = 50, ...styleArgs} = {}) {
     this._canvas.circle(
         x + this._xc,
@@ -75,7 +153,24 @@ export default class CanvasHandler {
         r,
         this._element_style(styleArgs))
   }
-  
+
+  /**
+   * Draws an ellipse.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.ellipse({x: -10, y: -10, w: 20, h: 20, fill:true})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=-50
+   * @param {Number} obj.y=-25
+   * @param {Number} obj.w=100
+   * @param {Number} obj.h=50
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.ellipse
+   * @function
+   **/
   ellipse({x = -50, y = -25, w = 100, h = 50, ...styleArgs} = {}) {
     this._canvas.ellipse(
         x + this._xc,
@@ -85,6 +180,32 @@ export default class CanvasHandler {
         this._element_style(styleArgs))
   }
   
+  /**
+   * Draws a fixation dot. The default style is medium-open.
+   * 
+   * - 'large-filled' is a filled circle with a 16px radius.
+   * - 'medium-filled' is a filled circle with an 8px radius.
+   * - 'small-filled' is a filled circle with a 4px radius.
+   * - 'large-open' is a filled circle with a 16px radius and a 2px hole.
+   * - 'medium-open' is a filled circle with an 8px radius and a 2px hole.
+   * - 'small-open' is a filled circle with a 4px radius and a 2px hole.
+   * - 'large-cross' is 16px cross.
+   * - 'medium-cross' is an 8px cross.
+   * - 'small-cross' is a 4px cross.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.fixdot()
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {String} obj.style='default'
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.fixdot
+   * @function
+   **/
   fixdot({x = 0, y = 0, style = 'default', ... styleArgs} = {}) {
     this._canvas.fixdot(
         x + this._xc,
@@ -93,17 +214,32 @@ export default class CanvasHandler {
         this._element_style(styleArgs))
   }
   
-  gabor({
-      x = 0,
-      y = 0,
-      orient = 0,
-      freq = .1,
-      env = 'gaussian',
-      size = 96,
-      stdev = 12,
-      phase = 0,
-      col1 = 'white',
-      col2 = 'black',
+  /**
+   * Draws a gabor patch.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.gabor({x: 100, y: 100, orient: 45, freq: .05})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {Number} obj.orient=0
+   * @param {Number} obj.freq=.1
+   * @param {String} obj.env='gaussian'
+   * @param {Number} obj.size=96
+   * @param {Number} obj.stdev=12
+   * @param {Number} obj.phase=0
+   * @param {String} obj.col1='white'
+   * @param {String} obj.col2='black'
+   * @param {String} obj.bgmode='avg'
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.gabor
+   * @function
+   **/
+  gabor({x = 0, y = 0, orient = 0, freq = .1, env = 'gaussian', size = 96,
+      stdev = 12, phase = 0, col1 = 'white', col2 = 'black',
       bgmode = 'avg'} = {}) {
     this._canvas.gabor(
         x + this._xc,
@@ -119,6 +255,25 @@ export default class CanvasHandler {
         bgmode)
   }
   
+  /**
+   * Draws an image from a file in the file pool.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.image({fname: 'image_in_pool.png'})
+   *
+   * @param {Object} obj
+   * @param {String} obj.fname
+   * @param {Boolean} obj.center=true
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {Number} obj.scale=1
+   * @param {Number} obj.rotation=0
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.image
+   * @function
+   **/
   image({fname, center = true, x = 0, y = 0, scale = 1, rotation = 0} = {}) {
     if (typeof fname === "undefined") {
       throw "fname is a required parameter for Canvas.image()"
@@ -132,6 +287,25 @@ export default class CanvasHandler {
         rotation)
   }
   
+  /**
+   * Draws a line.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * var ex = vars.width / 2
+   * var ey = vars.height / 2
+   * myCanvas.line({sx: 0, sy: 0, ex: ex, ey: ey})
+   * 
+   * @param {Object} obj
+   * @param {Number} obj.sx=0
+   * @param {Number} obj.sy=0
+   * @param {Number} obj.ex=50
+   * @param {Number} obj.ey=0
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.line
+   * @function
+   **/
   line({sx = 0, sy = 0, ex = 50, ey = 0, ...styleArgs} = {}) {
     this._canvas.line(
         sx + this._xc,
@@ -141,15 +315,29 @@ export default class CanvasHandler {
         this._element_style(styleArgs))
   }
   
-  noise_patch({
-      x = 0,
-      y = 0,
-      env = 'gaussian',
-      size = 96,
-      stdev = 12,
-      col1 = 'white',
-      col2 = 'black',
-      bgmode = 'avg'} = {}) {
+  /**
+   * Draws a patch of noise, with an envelope.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.noise_patch({x: 100, y: 100, env: 'circular'})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {String} obj.env='gaussian'
+   * @param {Number} obj.size=96
+   * @param {Number} obj.stdev=12
+   * @param {String} obj.col1='white'
+   * @param {String} obj.col2='black'
+   * @param {String} obj.bgmode='avg'
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.noise_patch
+   * @function
+   **/
+  noise_patch({x = 0, y = 0, env = 'gaussian', size = 96, stdev = 12,
+      col1 = 'white', col2 = 'black', bgmode = 'avg'} = {}) {
     this._canvas.noise(
         x + this._xc,
         y + this._yc,
@@ -161,6 +349,24 @@ export default class CanvasHandler {
         bgmode)
   }
   
+  /**
+   * Draws a polygon that defined by a list of vertices. I.e. a shape of
+   * points connected by lines.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * var n1 = [0,0]
+   * var n2 = [100, 100]
+   * var n3 = [0, 100]
+   * myCanvas.polygon({vertices: [n1, n2, n3]})
+   *
+   * @param {Object} obj
+   * @param {Array<Array<Number>>} obj.vertices
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.polygon
+   * @function
+   **/
   polygon({vertices, ...styleArgs} = {}) {
     if (typeof vertices === "undefined") {
       throw "vertices is a required parameter for Canvas.polygon()"
@@ -171,7 +377,24 @@ export default class CanvasHandler {
       v.push([x + this._xc, y + this._yc])
     this._canvas.polygon(v, this._element_style(styleArgs))
   }
-  
+
+  /**
+   * Draws a rectangle.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.rect({x: -10, y: -10, w: 20, h: 20, fill:true})
+   *
+   * @param {Object} obj
+   * @param {Number} obj.x=-50
+   * @param {Number} obj.y=-25
+   * @param {Number} obj.w=100
+   * @param {Number} obj.h=50
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.rect
+   * @function
+   **/
   rect({x = -50, y = -25, w = 100, h = 50, ...styleArgs} = {}) {
     this._canvas.rect(
         x + this._xc,
@@ -181,10 +404,36 @@ export default class CanvasHandler {
         this._element_style(styleArgs))
   }
   
+  /**
+   * Shows, or 'flips', the canvas on the screen.
+   *
+   * @return <Number> A timestamp of the time at which the canvas appeared on
+   *     the screen.
+   *
+   * @name Canvas.show
+   * @function
+   **/ 
   show() {
     return this._canvas.show()
   }
   
+  /**
+   * Draws text.
+   *
+   * @example
+   * var myCanvas = Canvas()
+   * myCanvas.text({text: 'Some text'})
+   *
+   * @param {Object} obj
+   * @param {String} obj.text
+   * @param {Boolean} obj.center=true
+   * @param {Number} obj.x=0
+   * @param {Number} obj.y=0
+   * @param {Object} ..obj.styleArgs={}
+   *
+   * @name Canvas.text
+   * @function
+   **/
   text({text, center = true, x = 0, y = 0, ...styleArgs} = {}) {
     if (typeof text === "undefined") {
       throw "text is a required parameter for Canvas.text()"
--- src/js/osweb/classes/javascript_workspace_api.js
+++ src/js/osweb/classes/javascript_workspace_api.js
@@ -1,4 +1,3 @@
-import randomExt from 'random-ext'
 import {range} from 'pythonic'
 
 /**
@@ -10,20 +9,78 @@ export default class JavaScriptWorkspaceAPI {
     this._experiment = experiment
   }
 
+  /**
+   * Resets all feedback variables to their initial state.
+   *
+   * @example
+   * reset_feedback()
+   **/
   reset_feedback() {
     this._experiment.reset_feedback()
   }
 
+  /**
+   * Sets the subject number and parity (even/ odd). This function is called
+   * automatically when an experiment is started, so you only need to call it
+   * yourself if you overwrite the subject number that was specified when the
+   * experiment was launched.
+   *
+   * @param {Number} nr The subject number
+   *
+   * @example
+   * set_subject_nr(1)
+   * console.log('Subject nr = ' + vars.subject_nr)
+   * console.log('Subject parity = ' + vars.subject_parity)
+   **/
   set_subject_nr(nr) {
     this._experiment.set_subject(nr)
   }
 
+  /**
+   * Returns true with a certain probability. (For more advanced randomization,
+   * use the `random-ext` package, which is available as `random`.)
+   * 
+   * @param {Number} [p=.5] The probability of returning true
+   *
+   * @example
+   * if (sometimes()) {
+   *   console.log('Sometimes you win')
+   * } else {
+   *   console.log('Sometimes you lose')
+   * }
+   **/
   sometimes(p=.5) {
     if (typeof p !== "number" || p < 0 || p > 1)
       throw "p should be a numeric value between 0 and 1"
     return Math.random() < p
   }
 
+  /**
+   * Converts polar coordinates (distance, angle) to Cartesian coordinates
+   * (x, y).
+   *
+   * @param {Number} rho The radial coordinate, also distance or eccentricity.
+   * @param {Number} phi The angular coordinate. This reflects a clockwise
+   *     rotation in degrees (i.e. not radians), where 0 is straight right.
+   * @param {Array<Number>} [pole=[0, 0]] The reference point.
+   * @return {Array<Number>} An [x, y] array.
+   *
+   * @example
+   * // ECMA 5.1
+   * var xy1 = xy_from_polar(100, 45)
+   * var xy2 = xy_from_polar(100, -45)
+   * var c = Canvas()
+   * c.line(xy1[0], xy1[1], -xy1[0], -xy1[1])
+   * c.line(xy2[0], xy2[1], -xy2[0], -xy2[1])
+   * c.show()
+   * // ECMA 6
+   * let x1, y1 = xy_from_polar(100, 45)
+   * let x2, y2 = xy_from_polar(100, -45)
+   * let c = Canvas()
+   * c.line(x1, y1, -x1, -y1)
+   * c.line(x2, y2, -x2, -y2)
+   * c.show()
+   **/
   xy_from_polar(rho, phi, pole=[0, 0]) {
     if (typeof rho !== "number")
       throw "rho should be numeric in xy_from_polar()"
@@ -36,6 +93,26 @@ export default class JavaScriptWorkspaceAPI {
     return [x, y]
   }
 
+  /**
+   * Converts Cartesian coordinates (x, y) to polar coordinates (distance,
+   * angle).
+   *
+   * @param {Number} x The X coordinate.
+   * @param {Number} y The Y coordinate
+   * @param {Array<Number>} [pole=[0, 0]] The reference point.
+   * @return {Array<Number>} An [rho, phi] array. Here, `rho` is the radial
+   *     coordinate, also distance or eccentricity. `phi` is the angular
+   *     coordinate in degrees (i.e. not radians), and reflects a
+   *     counterclockwise rotation, where 0 is straight right.
+   *
+   * @example
+   * // ECMA 5.1 (browser + desktop)
+   * var rho_phi = xy_to_polar(100, 100)
+   * var rho = rho_phi[0]
+   * var phi = rho_phi[1]
+   * // ECMA 6 (browser only)
+   * let rho, phi = xy_to_polar(100, 100)
+   **/
   xy_to_polar(x, y, pole=[0, 0]) {
     if (typeof x !== "number")
       throw "x should be numeric in xy_to_polar()"
@@ -49,6 +126,15 @@ export default class JavaScriptWorkspaceAPI {
     return [rho, phi]
   }
   
+  /**
+   * Gives the distance between two points.
+   *
+   * @param {Number} x1 The x coordinate of the first point.
+   * @param {Number} y1 The y coordinate of the first point.
+   * @param {Number} x2 The x coordinate of the second point.
+   * @param {Number} y2 The y coordinate of the second point.
+   * @return {Number} The distance between the two points.
+   **/
   xy_distance(x1, y1, x2, y2) {
     if (typeof x1 !== "number" || typeof y1 !== "number" || 
         typeof x2 !== "number" || typeof y2 !== "number")
@@ -56,6 +142,39 @@ export default class JavaScriptWorkspaceAPI {
     return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
   }
   
+  /**
+   * Generates a list of points (x,y coordinates) in a circle. This can be
+   * used to draw stimuli in a circular arrangement.
+   *
+   * @param {Number} n The number of x,y coordinates to generate.
+   * @param {Number} rho The radial coordinate, also distance or eccentricity,
+   *     of the first point.
+   * @param {Number} [phi0=0] The angular coordinate for the first coordinate.
+   *     This is a counterclockwise rotation in degrees (i.e. not radians),
+   *     where 0 is straight right.
+   * @param {Array<Number>} [pole=[0, 0]] The reference point.
+   * @return {Array<Array<Number>>} An array of [x,y] coordinate arrays.
+   *
+   * @example
+   * // Draw 8 rectangles around a central fixation dot
+   * // ECMA 5.1 (browser + desktop)
+   * var c = Canvas()
+   * c.fixdot()
+   * var points = xy_circle(8, 100)
+   * for (var i in points) {
+   *   var x = points[i][0]
+   *   var y = points[i][0]
+   *   c.rect(x - 10, y - 10, 20, 20)
+   * }
+   * c.show()
+   * // ECMA 6 (browser only)
+   * let c = Canvas()
+   * c.fixdot()
+   * for (let [x, y] of xy_circle(8, 100)) {
+   *   c.rect(x - 10, y - 10, 20, 20)
+   * }
+   * c.show()
+   **/
   xy_circle(n, rho, phi0=0, pole=[0, 0]) {
     if (typeof n !== "number" || n < 0)
       throw "n should be a non-negative integer in xy_circle()"
@@ -69,6 +188,38 @@ export default class JavaScriptWorkspaceAPI {
     return a
   }
   
+  /**
+   * Generates a list of points (x,y coordinates) in a grid. This can be used
+   * to draw stimuli in a grid arrangement.
+   *
+   * @param {Number|Array<Number>} n A number that indicates the number of
+   *     columns and rows, so that `n=2` indicates a 2x2 grid, or a [n_col,
+   *     n_row] array, so that `n=[2,3]` indicates a 2x3 grid.
+   * @param {Number|Array<Number>} spacing A numeric value that indicates the
+   *     spacing between cells, or a [col_spacing, row_spacing] array.
+   * @param {Array<Number>} [pole=[0, 0]] The reference point.
+   * @return {Array<Array<Number>>} An array of [x,y] coordinate arrays.
+   *
+   * @example
+   * // Draw a 4x4 grid of rectangles
+   * // ECMA 5 (desktop + browser)
+   * var c = Canvas()
+   * c.fixdot()
+   * var points = xy_grid(4, 100)
+   * for (var i in points) {
+   *   var x = points[i][0]
+   *   var y = points[i][0]
+   *   c.rect(x - 10, y - 10, 20, 20)
+   * }
+   * c.show()
+   * // ECMA 6 (browser only)
+   * let c = Canvas()
+   * c.fixdot()
+   * for (let [x, y] in xy_grid(4, 100)) {
+   *   c.rect(x-10, y-10, 20, 20)
+   * }
+   * c.show()
+   **/
   xy_grid(n, spacing, pole=[0, 0]) {
     // Parse and validate the n
     const n_err_msg = "n should be a single non-negative number, or an array of two non-negative numbers in xy_grid()"
@@ -113,6 +264,40 @@ export default class JavaScriptWorkspaceAPI {
     return a
   }
   
+  /**
+   * Generates a list of random points (x,y coordinates) with a minimum
+   * spacing between each pair of points. This function will throw an error
+   * when the coordinate list cannot be generated, typically because there are
+   * too many points, the min_dist is set too high, or the width or height are
+   * set too low.
+   *
+   * @param {Number} n The number of points to generate.
+   * @param {Number} width The width of the field with random points.
+   * @param {Number} height The height of the field with random points.
+   * @param {Number} [min_dist=0] The minimum distance between each point.
+   * @param {Array<Number>} [pole=[0, 0]] The reference point.
+   * @return {Array<Array<Number>>} An array of [x,y] coordinate arrays.
+   *
+   * @example
+   * // Draw a 50 rectangles in a random grid
+   * // ECMA 5 (desktop + browser)
+   * var c = Canvas()
+   * c.fixdot()
+   * var points = xy_random(50, 500, 500, 40)
+   * for (var i in points) {
+   *   var x = points[i][0]
+   *   var y = points[i][0]
+   *   c.rect(x - 10, y - 10, 20, 20)
+   * }
+   * c.show()   
+   * // ECMA 6 (browser only)
+   * let c = Canvas()
+   * c.fixdot()
+   * for (let [x, y] of xy_random(50, 500, 500, 40)) {
+   *   c.rect(x-10, y-10, 20, 20)
+   * }
+   * c.show()
+   **/
   xy_random(n, width, height, min_dist=0, pole=[0, 0]) {
     if (typeof n !== "number" || n < 0)
       throw "n should be a non-negative number in xy_circle()"
